3c9e5956e9531c23e3f98968ca6086e84d633f2a
listedmile <listedmile@gmail.com>
2026-04-04 08:59:21 +0530

    acure cofig file

--- frontend/apiUrl.jsx
+++ frontend/apiUrl.jsx
@@ -1,3 +1,3 @@
 // export const apiURL = "http://localhost:3000";
-export const apiURL = "https://hire-sphere.onrender.com";
+export const apiURL = "backendhiresphere-debsf0czfbardte9.centralindia-01.azurewebsites.net";
 // //  export const apiURL = "http://192.168.1.100:3000";
